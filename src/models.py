5ead8b4116ac592c8167ddb18821805e158be121
Bianca Szecowka <bianca.isl@gmail.com>
2023-11-13 14:23:04 +0000

    models y png

--- src/models.py
+++ src/models.py
@@ -7,26 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    reply = relationship("Reply")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Reply(Base):
+    __tablename__ = 'reply'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    reply_text = Column(String(500), nullable=False)
+    reply_id = Column(Integer, ForeignKey('reply.id')) 
+    person_id = Column(Integer, ForeignKey('user.id'))  
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    post_id = (Integer) 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Multimedia(Base):
+    __tablename__ = 'multimedia'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(30))
+    url= Column(String(200))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post) 
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column( Integer, ForeignKey('user.id'))
 
 ## Draw from SQLAlchemy base
 try:
@@ -35,3 +52,5 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+
